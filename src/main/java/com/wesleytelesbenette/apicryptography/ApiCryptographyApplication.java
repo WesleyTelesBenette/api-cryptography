ddddac1c044411713735263b33abab2011572a3c
Wesley T. Benette <weluonf@gmail.com>
2024-02-28 18:47:24 -0300

    Criação do Projeto.

---
+++ src/main/java/com/wesleytelesbenette/apicryptography/ApiCryptographyApplication.java
@@ -0,0 +1,13 @@
+package com.wesleytelesbenette.apicryptography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCryptographyApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ApiCryptographyApplication.class, args);
+	}
+}
